7a29a9d92e173e760816ba13d18a84f3588b6336
Alec Gheeraert <gheeraert.alec@gmail.com>
2024-01-28 23:11:31 +0100

    Final research code

--- controller.py
+++ controller.py
@@ -1,19 +1,12 @@
-from random import *
+from random import uniform
+import numpy as np
 from pyray import *
 from math import *
+from gymnasium import spaces, Env
 
-class _ObservationSpace:
-    def __init__(self):
-        self.position_x = 0
-        self.position_y = 1
-        self.position_z = 2
-        self.velocity_x = 3
-        self.velocity_y = 4
-        self.velocity_z = 5
-        self.time = 6
-
-class Controller:
-    def __init__(self, cruise_velocity, dt, render=True, fps=20):
+class Controller(Env):
+    metadata = {"render_modes": ["human"]}
+    def __init__(self, cruise_velocity, dt, fps=20, render_mode=None):
         """
         Parameters
         ----------
@@ -26,203 +19,152 @@ class Controller:
         fps : int, optional
             The rendering fps (default is 20)
         """
+        super(Controller, self).__init__()
         self._cruise_velocity = cruise_velocity
         self._dt = dt
-        self._render = render
-        # init renderer
+        self.render_mode = render_mode
         self.__screen_width = 800
         self.__screen_height = 400
-        self.__screen_title = 'cipher'
+        self.__screen_title = 'research'
         self.__screen_fps = fps
-        # raylib
-        if render:
+
+        if self.render_mode == "human":
             init_window(self.__screen_width, self.__screen_height, self.__screen_title)
             set_target_fps(self.__screen_fps)
-        # set wc
-        self._observation_space = _ObservationSpace()
-        self._action_space = self.__action_space()
+
+        self.observation_space = self._get_observation_space()
+        self.action_space = spaces.Discrete(4)
         self.reset()
 
-    def reset(self):
-        resistance_drag = 5.0
-        resistance_roll = 30.0
-        force_engine = 8000.0
-        # calculate cruise forces
-        drag_x = resistance_drag * self._cruise_velocity * abs(self._cruise_velocity)
-        roll_x = resistance_roll * self._cruise_velocity
-        # time (s)
-        self.__wc_time = 0.0
-        # controls
-        self.__wc_throttle = (drag_x + roll_x) / force_engine
-        self.__wc_brake = 0.0
-        self.__wc_steering = 0.0
-        # position (m, m, rads)
-        self.__wc_position_x = 0.0
-        self.__wc_position_y = 0.0
-        self.__wc_position_z = 0.0
-        # velocity (m/s, m/s, rads/s)
-        self.__wc_velocity_x = self._cruise_velocity
-        self.__wc_velocity_y = 0.0
-        self.__wc_velocity_z = 0.0
-        # acceleration (m/s^2, m/s^2)
-        self.__wc_acceleration_x = 0.0
-        self.__wc_acceleration_y = 0.0
-        # run physics
-        self.__slip()
-        for _ in range(3):
-            self.__physics(True)
+    def reset(self, seed=None):
+        """
+        Returns
+        -------
+        observation : array
+            The initial observation
+        info : dict
+            Additional information
+        """
+        super().reset(seed=seed)
+        resistance = - 5.0 * self._cruise_velocity * abs(self._cruise_velocity) - 30.0 * self._cruise_velocity
+
+        self._position_x_wc = 0.0
+        self._position_y_wc = 0.0
+        self._velocity_x = self._cruise_velocity
+        self._velocity_y = 0.0
+        self._acceleration_x = 0.0
+        self._acceleration_y = 0.0
+        self._time = 0.0
+
+        self._throttle = 0.0
+        self._brake = 0.0
+        self._delta = 0.0
+        self._theta = 0.0
+        self._omega = 0.0
+
+        self.__rewards = 0.0
+
+        self._slip()
+        for _ in range(4):
+            self._physics()
+        self._throttle = 0.0
+
+        return self._observation(), self._get_info()
+    
+    def step(self, action):
+        """
+        Parameters
+        ----------
+        action : int
+            The action to take
         
-        # clear renderer
-        return self.__observation()
+        Returns
+        -------
+        observation : array
+            The observation after the action is taken
+        reward : float
+            The reward after the action is taken
+        done : bool
+            If the episode is done
+        info : dict
+            Additional information
+        """
+        self._action(action)
+        self._physics()
 
-
-    def step(self, throttle, brake, steering):
-        # filter controls
-        # TODO smoother controls
-        self.__wc_throttle = max(min(throttle, 1.0), 0.0)
-        self.__wc_brake = max(min(brake, 1.0), 0.0)
-        self.__wc_steering = max(min(steering, 0.65), -0.65) * -1.0
-        # run physics
-        self.__physics()
-        # filter observation
-        # return observation, reward, terminated, truncated
-        observation = self.__observation()
-        terminated = self.__terminated(observation)
-        truncated = self.__truncated(observation)
-        reward = self.__reward(observation, terminated, truncated)
-        return observation, reward, terminated, truncated
+        observation = self._observation()
+        terminated = self._terminated(observation)
+        truncated = self._truncated(observation)
+        self._reward(observation, terminated)
+        return observation, self.__rewards, terminated, truncated, self._get_info()
 
     def render(self):
-        if self._render:
+        if self.render_mode == "human":
             if window_should_close():
                 close_window()
                 return False
-            self.__render()
+            else:
+                self._render()
 
-    def __physics(self, slip=False):
-        # constants
-        mass = 1500
-        inertia = 1500
-        cg_front = 1.25
-        cg_rear = 1.25
-        grip_front = 2.0
-        grip_rear = 2.0
-        stiffness_front = 5.0
-        stiffness_rear = 5.2
-        resistance_drag = 5.0
-        resistance_roll = 30.0
-        force_engine = 8000.0
-        force_brake = 10000.0
-        wheel_base = cg_front + cg_rear
-        cg_front_ratio = cg_front / wheel_base
-        cg_rear_ratio = cg_rear / wheel_base
-        # calculate position sin and cos
-        position_sn = sin(self.__wc_position_z)
-        position_cs = cos(self.__wc_position_z)
-        # transform into local velocity
-        velocity_x = position_cs * self.__wc_velocity_x + position_sn * self.__wc_velocity_y
-        velocity_y = position_cs * self.__wc_velocity_y - position_sn * self.__wc_velocity_x
-        
-        # calculate angular velocity on each axle
-        angular_front = cg_front * self.__wc_velocity_z
-        angular_rear = -cg_rear * self.__wc_velocity_z
-        # calculate slip angles on each axle
-        slip_front = atan2(velocity_y + angular_front, abs(velocity_x)) - self.__wc_steering * copysign(1.0, velocity_x)
-        slip_rear = atan2(velocity_y + angular_rear, abs(velocity_x))
-        # calculate lateral forces on each axle
-        lateral_front = min(max(stiffness_front * slip_front, -grip_front), grip_front) * (mass * cg_front_ratio)
-        lateral_rear = min(max(stiffness_rear * slip_rear, -grip_rear), grip_rear) * (mass * cg_rear_ratio)
+    def _action(self, action):
+        chosen = self._get_action_space(action)
+        if chosen[0] == 0.0:
+            if self._throttle >= 0.2:
+                self._throttle -= 0.2
+        if chosen[0] == 1.0:
+            if self._throttle <= 0.8:
+                self._throttle += 0.2
+        if chosen[1] == 0.0:
+            if self._brake >= 0.2:
+                self._brake -= 0.2
+        if chosen[1] == 1.0:
+            if self._brake <= 0.8:
+                self._brake += 0.2
+        if chosen[2] == 1.0:
+            if self._delta <= 0.5:
+                self._delta += 0.1
+        if chosen[2] == -1.0:
+            if self._delta >= -0.5:
+                self._delta -= 0.1
 
-        # calculate drag resistance for each axes
-        drag_x = -resistance_drag * velocity_x * abs(velocity_x)
-        drag_y = -resistance_drag * velocity_y * abs(velocity_y)
-        # calculate rolling resistance for each axes
-        roll_x = -resistance_roll * velocity_x
-        roll_y = -resistance_roll * velocity_y
-        # calculate traction force for each axes
-        traction_x = (force_engine * self.__wc_throttle) - (force_brake * self.__wc_brake) * copysign(1.0, velocity_x)
-        traction_y = 0.0
+    def _physics(self):
+        self.__f_x = 0.0
+        self.__f_y = 0.0
+        self.__a_y = 0.0
 
-        if slip:
-            lateral_front *= self.__slip_lateral_front
-            lateral_rear *= self.__slip_lateral_rear
-            traction_x *= self.__slip_traction_x
-            print(lateral_front, lateral_rear, traction_x, self.__wc_steering)
+        drag_x = - 5.0 * self._velocity_x * abs(self._velocity_x)
+        drag_y = - 5.0 * self._velocity_y * abs(self._velocity_y)
+        roll_x = - 30.0 * self._velocity_x
+        roll_y = - 30.0 * self._velocity_y
 
-        # calculate total force on each axes
-        force_x = drag_x + roll_x + traction_x
-        force_y = drag_y + roll_y + traction_y + cos(self.__wc_steering) * lateral_front + lateral_rear
-        # calculate acceleration on each axes
-        acceleration_x = force_x / mass
-        acceleration_y = force_y / mass
-        # calculate angular torque
-        angular_torque = cos(self.__wc_steering) * lateral_front * cg_front - lateral_rear * cg_rear
-
-        self.__wc_acceleration_x = position_cs * acceleration_x - position_sn * acceleration_y
-        self.__wc_acceleration_y = position_sn * acceleration_x + position_cs * acceleration_y
-        self.__wc_velocity_x += self._dt * self.__wc_acceleration_x
-        self.__wc_velocity_y += self._dt * self.__wc_acceleration_y
-        self.__wc_velocity_z += (angular_torque / inertia) * self._dt
-        self.__wc_position_x += (velocity_x * self._dt) + (self.__wc_acceleration_x * self._dt * self._dt) * 0.5
-        self.__wc_position_y += (velocity_y * self._dt) + (self.__wc_acceleration_y * self._dt * self._dt) * 0.5        
-        self.__wc_position_z = (self.__wc_position_z + (self._dt * self.__wc_velocity_z * self._dt) * 0.5) % (2 * pi)
-        self.__wc_time += self._dt
-
-    def __slip(self):
-        self.__slip_lateral_front = round(uniform(0.8, 1.0), 3)
-        self.__slip_lateral_rear = round(uniform(0.6, 0.8), 3)
-        self.__slip_traction_x = round(uniform(0.6, 0.8), 3)
-        if randint(0, 1) == 0:
-            self.__wc_steering = round(uniform(0.2, 0.3), 2)
+        beta = atan(tan(self._delta) * 0.5)
+        self.__f_x = self._throttle * 4000 - self._brake * 10000
+        if self._delta:
+            r = 2.6 / tan(beta)
+            self.__a_y = self._velocity_x ** 2 / r
+            self.__f_y = self.__a_y * 1500.0
+            self._omega = self._velocity_x / r
         else:
-            self.__wc_steering = round(uniform(-0.3, -0.2), 2)
+            self._omega /= 1.1
 
-    def __action_space(self):
-        return [
-            [0.0, 0.0, 0.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 1.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 1.0],
-        ]
+        if self.__f_y > 15000:
+            self._omega *= 2.5
 
-    # observations
-    def __observation(self):
-        # nc_position_y = round((self.__wc_position_y / 1.6), 3)
-        # nc_position_z = round(copysign(cos(self.__wc_position_z), self.__wc_velocity_z), 5)
-        position_x = round(self.__wc_position_x, 3)
-        position_y = round(self.__wc_position_y, 3)
-        position_z = round(copysign(self.__wc_position_z, self.__wc_velocity_z), 3)
-        velocity_x = round(self.__wc_velocity_x, 3)
-        velocity_y = round(self.__wc_velocity_y, 3)
-        velocity_z = round(self.__wc_velocity_z, 3)
-        time = round(self.__wc_time, 3)
-        return [position_x, position_y, position_z, velocity_x, velocity_y, velocity_z, time]
-    
-    def __reward(self, observation, terminated, truncated):
-        reward = 0.0
-        if terminated:
-            reward += -100.0
-        if truncated:
-            reward += -20.0
-        if abs(observation[1]) > 0.5:
-            reward += 10.0
-        return
+        self._acceleration_x = (drag_x + roll_x + self.__f_x) / 1500.0
+        self._acceleration_y = (drag_y + roll_y + self.__f_y * sin(beta)) / 1500.0
+        self._velocity_x += self._acceleration_x * self._dt
+        self._velocity_y += self._acceleration_y * self._dt
 
-    def __terminated(self, observation):
-        if observation[self._observation_space.position_y] > 4.8 or observation[self._observation_space.position_y] < -4.8:
-            return True
-        elif observation[self._observation_space.time] >= 30.0:
-            return True
-        return False
-    
-    def __truncated(self, observation):
-        if observation[self._observation_space.position_x] >= 300.0:
-            return True
-        return False
-    
-    def __render(self):
+        self._theta = (self._theta + self._omega * self._dt) % (2 * pi)
+        self._time += self._dt     
+        
+        self._position_x_wc += (cos(self._theta) * self._velocity_x - sin(self._theta) * self._velocity_y) * self._dt
+        self._position_y_wc += (sin(self._theta) * self._velocity_x + cos(self._theta) * self._velocity_y) * self._dt
+
+    def _slip(self):
+        self._omega = uniform(-3, 3)
+
+    def _render(self):
         begin_drawing()
         clear_background(RAYWHITE)
         
@@ -232,27 +174,29 @@ class Controller:
         draw_text("acc:", 20, 80, 20, BLACK)
         draw_text("rad:", 20, 110, 20, BLACK)
 
-        draw_text(str(round(self.__wc_position_x, 2)), 80, 20, 20, BLACK)
-        draw_text(str(round(self.__wc_position_y, 2)), 200, 20, 20, BLACK)
+        draw_text(str(round(self._position_x_wc, 2)), 80, 20, 20, BLACK)
+        draw_text(str(round(self._position_y_wc, 2)), 200, 20, 20, BLACK)
 
-        draw_text(str(round(self.__wc_velocity_x, 2)), 80, 50, 20, BLACK)
-        draw_text(str(round(self.__wc_velocity_y, 2)), 200, 50, 20, BLACK)
+        draw_text(str(round(self._velocity_x, 2)), 80, 50, 20, BLACK)
+        draw_text(str(round(self._velocity_y, 2)), 200, 50, 20, BLACK)
+        draw_text(str(round(self._acceleration_x, 2)), 80, 80, 20, BLACK)
+        draw_text(str(round(self._acceleration_y, 2)), 200, 80, 20, BLACK)
+        draw_text(str(round(self._theta, 2)), 80, 110, 20, BLACK)
+        draw_text(str(round(self._omega, 2)), 200, 110, 20, BLACK)
 
-        draw_text(str(round(self.__wc_acceleration_x, 2)), 80, 80, 20, BLACK)
-        draw_text(str(round(self.__wc_acceleration_y, 2)), 200, 80, 20, BLACK)
-
-        draw_text(str(round(self.__wc_position_z, 2)), 80, 110, 20, BLACK)
-        draw_text(str(round(self.__wc_velocity_z, 2)), 200, 110, 20, BLACK)
+        draw_text(str(round(self._throttle, 2)), 200, 160, 20, BLACK)
+        draw_text(str(round(self._brake, 2)), 200, 180, 20, BLACK)
+        draw_text(str(round(self._delta, 2)), 200, 200, 20, BLACK)
 
         # timing
         draw_text("time:", 20, self.__screen_height - 40, 20, BLACK)
-        draw_text(str(round(self.__wc_time, 2)), 80, self.__screen_height - 40, 20, BLACK)
+        draw_text(str(round(self._time, 2)), 80, self.__screen_height - 40, 20, BLACK)
 
         # compass
         compass_x = self.__screen_width // 4 * 3
         compass_y = self.__screen_height // 2
         radius = self.__screen_height // 10
-        angle = self.__wc_position_z - 1.5708
+        angle = self._theta - 1.5708
         draw_line(compass_x, compass_y - radius * 3, compass_x, compass_y + radius * 3, BLACK)
         draw_line(compass_x - radius * 3, compass_y, compass_x + radius * 3, compass_y, BLACK)
         draw_triangle(
@@ -262,3 +206,82 @@ class Controller:
             BLACK
         )
         end_drawing()
+
+    def _observation(self):
+        return {
+            'position_x': np.array([round(self._position_x_wc, 3)]),
+            'position_y': np.array([round(self._position_y_wc, 3)]),
+            'velocity_x': np.array([round(self._velocity_x, 3)]),
+            'velocity_y': np.array([round(self._velocity_y, 3)]),
+            'angular_velocity': np.array([round(self._omega, 3)]),
+            'angle': np.array([round(self._theta, 3)])
+        }
+
+    def _terminated(self, observation):
+        if observation['position_y'] > 4.8 or observation['position_y'] < -4.8:
+            return True
+        elif self._time >= 30.0:
+            return True
+        return False
+
+    def _truncated(self, observation):
+        if observation['position_x'] >= 300.0:
+            return True
+        return False
+
+    def _reward(self, observation, terminated):
+        self.__rewards = self.__rewards - 10.0
+        if terminated:
+            self.__rewards -= 100.0
+        # rewards position y
+        if -1.6 < abs(observation['position_y']) < 1.6:
+            self.__rewards += 60.0
+        elif -3.2 < abs(observation['position_y']) < 3.2:
+            self.__rewards += 40.0
+        elif -4.8 < abs(observation['position_y']) < 4.8:
+            self.__rewards += 20.0
+        # rewards position z
+        if -1.0 < abs(observation['angle']) < 1.0:
+            self.__rewards += 60.0
+        elif -2.0 < abs(observation['angle']) < 2.0:
+            self.__rewards += 30.0
+        # rewards velocity z
+        if -0.1 < abs(observation['angular_velocity']) < 0.1:
+            self.__rewards += 60.0
+        elif -0.2 < abs(observation['angular_velocity']) < 0.2:
+            self.__rewards += 40.0
+        elif -0.4 < abs(observation['angular_velocity']) < 0.4:
+            self.__rewards += 20.0
+        elif -0.6 < abs(observation['angular_velocity']) < 0.6:
+            self.__rewards += 10.0
+        # rewards direction
+        if observation['position_x'] > 0.0:
+            self.__rewards += 10.0
+        else:
+            self.__rewards -= 50.0
+
+    def _get_observation_space(self):
+        return spaces.Dict({
+            'position_x': spaces.Box(low=-float('inf'), high=float('inf'), shape=(1,), dtype='float64'),
+            'position_y': spaces.Box(low=-float('inf'), high=float('inf'), shape=(1,), dtype='float64'),
+            'velocity_x': spaces.Box(low=-float('inf'), high=float('inf'), shape=(1,), dtype='float64'),
+            'velocity_y': spaces.Box(low=-float('inf'), high=float('inf'), shape=(1,), dtype='float64'),
+            'angular_velocity': spaces.Box(low=-float('inf'), high=float('inf'), shape=(1,), dtype='float64'),
+            'angle': spaces.Box(low=-float('inf'), high=float('inf'), shape=(1,), dtype='float64')
+        })
+
+    def _get_action_space(self, action):
+        actions = [
+            # [0.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0],
+            [0.0, 0.0, -1.0],
+            [1.0, 0.0, 1.0],
+            # [1.0, 0.0, 0.0],
+            [1.0, 0.0, -1.0],
+        ]
+        return actions[action]
+
+    def _get_info(self):
+        return {
+            "distance": 300 - self._position_x_wc
+        }
--- model.py
+++ model.py
@@ -1,25 +1,66 @@
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3 import PPO, DQN, A2C
 from controller import Controller
-from math import pi
-import time
 
-controller = Controller(20.0, 0.2)
+class Model:
+    def __init__(self, 
+        env, 
+        time_steps=30000, 
+        model_type="PPO",
+        verbose=1
+        ):
+        """
+        Parameters
+        ----------
+        env : Controller
+            The environment to train the model on.
+        time_steps : int, optional
+            The number of time steps to train the model for. The default is 30000.
+        model_type : str, optional
+            The type of model to train. The default is "PPO".
+        verbose : int, optional
+            The verbosity level. The default is 1.
+        """
+        self.env = DummyVecEnv([lambda: env])
+        self.model_type = model_type
+        self.time_steps = time_steps
+        self.verbose = verbose
+        self._models()
 
-for i in range(1):
-    print("[position_x, position_y, position_z, velocity_x, velocity_y, velocity_z, time]")
-    observation = controller.reset()
-    terminated = truncated = False
-    reward = 0.0
-    counter = 0
-    steering = 0.0
-    angle = observation[1]
-    while not terminated and not truncated:
-        counter += 1
-        if abs(angle) < pi:
-            steering = -0.3
+    def _models(self):  
+        if self.model_type == "PPO":
+            self.model = PPO("MultiInputPolicy", self.env, verbose=self.verbose)
+        elif self.model_type == "DQN":
+            self.model = DQN("MultiInputPolicy", self.env, verbose=self.verbose)
+        elif self.model_type == "A2C":
+            self.model = A2C("MultiInputPolicy", self.env, verbose=self.verbose)
         else:
-            steering = 0.3
-        observation, reward, terminated, truncated = controller.step(0, 0, steering)
-        print(observation, steering)
-        controller.render()
-        angle = observation[controller._observation_space.position_z]
-    print("Episode finished after {} timesteps".format(counter))
+            raise ValueError("Invalid model type.")
+
+    def learn(self):
+        self.model.learn(total_timesteps=self.time_steps)
+        self.model.save("research")
+
+    def evaluate(self):
+        loaded_model = PPO.load("research")
+        mean_reward, std_reward = evaluate_policy(loaded_model, self.env, n_eval_episodes=10)
+        print(mean_reward, std_reward)
+
+    def replay(self):
+        loaded_model = PPO.load("research")
+        obs = self.env.reset()
+        for _ in range(1000):
+            action, _ = loaded_model.predict(obs, deterministic=True)
+            obs, _, done, _ = self.env.step(action)
+            self.env.render()
+            if done:
+                obs = self.env.reset()
+
+
+model = Model(
+    Controller(20.0, 0.1, render_mode="human"),
+)
+model.learn()
+model.evaluate()
+model.replay()
